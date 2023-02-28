2b74a68aa701c82154c06219b9d3d7e633506d9a
juwez <joeylybaert@outlook.com>
2023-02-28 20:46:50 +0100

    wijzigingen commiten

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import  pygame
-print("hello world")
+import pygame
+import random # niet volledig zeker
+
+#GLOBALE VARS
+# 10 x 20 GRID
+
+col = 10 # 10 columns
+row = 20 # 20 rows
+s_width = 800 # window width
+s_height = 700 # window height
+play_width = 300 # speelveld width
+play_height = 600 # speelveld hoogte
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height - 50
+
+#dont know if it works
